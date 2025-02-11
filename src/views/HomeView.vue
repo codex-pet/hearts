298e1cb913776ab80cdf3fa0885c63f817af1a52
codex-pet <peterayono143@gmail.com>
2025-02-11 19:48:49 +0800

    Add .gitattributes to normalize line endings

---
+++ src/views/HomeView.vue
@@ -0,0 +1,215 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const showFirstContainer = ref(true);
+const yesButtonScale = ref(1); // Track scale size
+const noButtonTexts = ["No :<", "Sure nana ling?", "di najd mahanyo :<", "Last najud!", "mo iyak nako! 😢"];
+const noClickCount = ref(0); // Track clicks on the No button
+
+const toggleContainer = () => {
+  showFirstContainer.value = !showFirstContainer.value;
+};
+
+const enlargeYesButton = () => {
+  yesButtonScale.value += 1.5; // Increase scale size on each click
+  noClickCount.value = (noClickCount.value + 1) % noButtonTexts.length; // Cycle through messages
+};
+</script>
+
+<template>
+  <div class="container">
+    <!-- First Container -->
+    <div v-if="showFirstContainer" class="first-container" @click="toggleContainer">
+      <img src="@/assets/miming.png" alt="miming" />
+      <p class="text-name">For Langling <33</p>
+    </div>
+
+    <!-- Second Container -->
+    <div v-else class="second-container">
+      <div class="sub-container">
+        <img src="@/assets/miming2.png" alt="miming2" />
+        <p class="text-title">Will you <br /> be my <br /> valentine??</p>
+        <div class="btns">
+          <button class="yes" :style="{ transform: `scale(${yesButtonScale})` }" @click="router.push('/yes')">Yes!</button>
+          <button class="no" @click="enlargeYesButton">{{ noButtonTexts[noClickCount] }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Marck+Script&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Lobster&family=Marck+Script&display=swap");
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0 auto;
+  overflow-y: hidden;
+
+  
+  
+  @media screen and (max-width: 600px) {
+    margin: 0px 10px;
+  }
+}
+
+.first-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  height: 450px;
+  background-color: #ffb3c6;
+  cursor: pointer;
+  border-radius: 50px;
+
+  @media screen and (max-width: 600px) {
+    width: 400px;
+    height: 400px;
+    margin: 15px;
+  }
+
+  .text-name {
+    color: white;
+    font-size: 50px;
+    font-family: "Marck Script", serif;
+    font-weight: 400;
+    font-style: normal;
+
+    @media screen and (max-width: 600px) {
+      font-size: 30px;
+    }
+  }
+
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50px;
+
+    @media screen and (max-width: 600px) {
+      width: 100px;
+      height: 100px;
+    }
+  }
+}
+
+.second-container {
+  display: flex;
+  width: 500px;
+  height: 600px;
+  background-color: red;
+  cursor: pointer;
+  border-radius: 50px;
+  position: relative;
+  overflow: hidden;
+
+  @media screen and (max-width: 600px) {
+    width: 350px;
+    margin: 10px;
+  }
+
+  /* Flickering Heart Background */
+  &::before {
+    content: "❤";
+    font-size: 200px;
+    color: rgba(255, 255, 255, 0.6);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    animation: flicker 1.5s infinite alternate ease-in-out;
+  }
+}
+
+@keyframes flicker {
+  0% {
+    opacity: 0.3;
+    transform: translate(-50%, -50%) scale(1);
+  }
+  50% {
+    opacity: 1;
+    transform: translate(-50%, -50%) scale(1.1);
+  }
+  100% {
+    opacity: 0.3;
+    transform: translate(-50%, -50%) scale(1);
+  }
+}
+
+.sub-container {
+  display: flex;
+  flex-direction: column;
+  margin: 50px 20px;
+  padding: 20px;
+  cursor: pointer;
+  border-radius: 50px;
+  position: relative;
+
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50px;
+    position: absolute;
+    left: 53%;
+    top: 40px;
+
+    @media screen and (max-width: 600px) {
+      width: 200px;
+      height: 200px;
+      left: 40%;
+      top: 20px;
+    }
+  }
+
+  .text-title {
+    color: white;
+    font-size: 60px;
+    font-family: "Lobster", serif;
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  .btns {
+    display: flex;
+    margin-top: 150px;
+    justify-content: space-between;
+
+    .yes {
+      margin: 10px;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      background-color: white;
+      color: red;
+      font-size: 20px;
+      font-weight: bold;
+      cursor: pointer;
+      transition: transform 0.3s ease-in-out;
+    }
+
+    .no {
+      margin: 10px;
+      margin-left: 150px;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      background-color: white;
+      color: red;
+      font-size: 20px;
+      font-weight: bold;
+      cursor: pointer;
+
+      @media screen and (max-width: 600px) {
+        margin-left: 75px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/YesPageView.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+
+
+</script>
+
+<template>
+  <div class="container">
+    <!-- First Container -->
+     
+    <div class="first-container" >
+      <p class="text-name">YEYEYEYYY!!🥰🥰</p>
+      <p class="text-name">LABYUULABYUU😘</p>
+      <p class="text-name">BABYYYY!!!💞💞</p>
+
+      <div class="miming">
+        <img class="miming1" src="@/assets/mimingjump.gif" alt="miming" />
+        <img class="miming2" src="@/assets/mimingjump.gif" alt="miming" />
+      </div>
+
+    </div>
+
+    
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Itim&family=Lobster&family=Marck+Script&family=Pacifico&family=Shadows+Into+Light&display=swap');
+
+
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #ffb3c6;
+  margin: 0 auto;
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  }
+
+  .first-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 600px;
+    // height: 550px;
+    cursor: pointer;
+    border-radius: 50px;
+
+    @media screen and (max-width: 600px) {
+        width: 400px;
+        margin-top: 100px;
+    }
+
+    .text-name {
+      color: white;
+      font-size: 60px;
+      font-family: "Itim", serif;
+      font-weight: 400;
+      font-style: normal;
+      margin-top:20px;
+
+      @media screen and (max-width: 600px) {
+        font-size: 40px;
+      }
+      @media screen and (max-width: 360px) {
+        font-size: 30px;
+      }
+    }
+
+    .miming {
+        @media screen and (max-width: 600px) {
+        // display:flex;
+        margin-left:40px;
+      }
+      .miming1 {
+        width: 300px;
+        height: 300px;
+      }
+      .miming2 {
+        width: 300px;
+        height: 300px;
+      }
+    }
+  }
+
+  
+}
+</style>
